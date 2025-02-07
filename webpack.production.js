5ba23488b05fc1d8e36c2d31f2ecd851d999b10a
suqingyao <suqingyao333@gmail.com>
2025-02-07 09:53:26 +0800

    chore: optimize webpack production configuration with code splitting and output improvements

--- webpack.production.js
+++ webpack.production.js
@@ -1,9 +1,19 @@
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = merge(require('./webpack.common.js'), {
   mode: 'production',
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
   module: {
     rules: [
       {
@@ -13,21 +23,29 @@ module.exports = merge(require('./webpack.common.js'), {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: '/' // 设置你的公共资源路径。按需进行修改
-            }
+              publicPath: '/', // 设置你的公共资源路径。按需进行修改
+            },
           },
           ,
           'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
+          {
+            loader: 'postcss-loader',
+            options: {
+              // postcssOptions: {
+              //   plugins: [require('autoprefixer'), require('cssnano')],
+              // },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'css/[name][hash:7].css', // 配置输出的 CSS 文件名。可以自定义文件名和路径。
-      chunkFilename: 'css/[id].css'
-    })
-  ]
+      chunkFilename: 'css/[id].css',
+    }),
+  ],
 });
